04c26013fec89450e872ee5baa1e5a96207365e8
zhiyong0804 <517609429@qq.com>
2018-10-26 11:39:24 +0800

    add usage

--- src/main.cpp
+++ src/main.cpp
@@ -10,29 +10,226 @@
 #include <functional>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
 
+
+#define OPTERRCOLON (1)
+#define OPTERRNF (2)
+#define OPTERRARG (3)
+#define DEFAULT_BIN_NAME "brks"
+
+int optreset = 0;
+int optind = 1;
+int opterr = 1;
+int optopt;
+
+int brks_fprintf(FILE *file, const char *fmt,  ...)
+{
+    if (file == NULL)
+        return -1;
+
+    va_list args;
+    va_start(args,fmt);
+    int result = ::vfprintf(file, fmt, args);
+    va_end(args);
+
+    return result;
+}
+
+static int optiserr(int argc, char * const *argv, int oint, const char *optstr, int optchr, int err)
+{
+    if(opterr)
+    {
+        brks_fprintf(stderr, "Error in argument %d, char %d: ", oint, optchr+1);
+        switch(err)
+        {
+        case OPTERRCOLON:
+            brks_fprintf(stderr, ": in flags\n");
+            break;
+        case OPTERRNF:
+            brks_fprintf(stderr, "option not found %c\n", argv[oint][optchr]);
+            break;
+        case OPTERRARG:
+            brks_fprintf(stderr, "no argument for option %c\n", argv[oint][optchr]);
+            break;
+        default:
+            brks_fprintf(stderr, "unknown\n");
+            break;
+        }
+    }
+    optopt = argv[oint][optchr];
+    return('?');
+}
+
+static int getopt(int argc, char* const *argv, const char *optstr)
+{
+    static int optchr = 0;
+    static int dash = 0; /* have already seen the - */
+
+    char *cp;
+
+    if (optreset)
+        optreset = optchr = dash = 0;
+    if(optind >= argc)
+        return(EOF);
+    if(!dash && (argv[optind][0] !=  '-'))
+        return(EOF);
+    if(!dash && (argv[optind][0] ==  '-') && !argv[optind][1])
+    {
+        /*
+         * use to specify stdin. Need to let pgm process this and
+         * the following args
+         */
+        return(EOF);
+    }
+    if((argv[optind][0] == '-') && (argv[optind][1] == '-'))
+    {
+        /* -- indicates end of args */
+        optind++;
+        return(EOF);
+    }
+    if(!dash)
+    {
+        assert((argv[optind][0] == '-') && argv[optind][1]);
+        dash = 1;
+        optchr = 1;
+    }
+
+    /* Check if the guy tries to do a -: kind of flag */
+    assert(dash);
+    if(argv[optind][optchr] == ':')
+    {
+        dash = 0;
+        optind++;
+        return(optiserr(argc, argv, optind-1, optstr, optchr, OPTERRCOLON));
+    }
+    if(!(cp = strchr(optstr, argv[optind][optchr])))
+    {
+        int errind = optind;
+        int errchr = optchr;
+
+        if(!argv[optind][optchr+1])
+        {
+            dash = 0;
+            optind++;
+        }
+        else
+            optchr++;
+        return(optiserr(argc, argv, errind, optstr, errchr, OPTERRNF));
+    }
+    if(cp[1] == ':')
+    {
+        dash = 0;
+        optind++;
+        if(optind == argc)
+            return(optiserr(argc, argv, optind-1, optstr, optchr, OPTERRARG));
+        optarg = argv[optind++];
+        return(*cp);
+    }
+    else
+    {
+        if(!argv[optind][optchr+1])
+        {
+            dash = 0;
+            optind++;
+        }
+        else
+            optchr++;
+        return(*cp);
+    }
+    assert(0);
+    return(0);
+}
+
+static int daemon(int nochdir, int noclose)
+{
+    int fd;
+
+    switch (fork())
+    {
+        case -1:
+            return (-1);
+        case 0:
+            break;
+        default:
+            _exit(0);
+    }
+
+    if (setsid() == -1)
+        return (-1);
+
+    if (!nochdir)
+        (void)chdir("/");
+
+    if (!noclose && (fd = open("/dev/null", O_RDWR, 0)) != -1) {
+        (void)dup2(fd, STDIN_FILENO);
+        (void)dup2(fd, STDOUT_FILENO);
+        (void)dup2(fd, STDERR_FILENO);
+        if (fd > 2)
+            (void)close (fd);
+    }
+
+    return (0);
+}
+
+static void usage()
+{
+    char* usage_name = DEFAULT_BIN_NAME;
+    printf("usage: %s [ -d | -v | -c brks.ini | -l log.conf ]\n", usage_name);
+    printf("-d: Run in the foreground\n");
+    printf("-c brks.ini: Specify a config file\n");
+    printf("-l log.conf: Specify a log config file\n");
+}
 
 int main(int argc, char** argv)
 {
-    if (argc != 3)
+    if (argc <= 1)
     {
-        printf("please input brks <log file config> <config file path>!\n");
-        return -1;
+        usage();
     }
 
-    if(!Logger::instance()->init(std::string(argv[1])))
+    char ch;
+    bool run_in_foreground = false;
+    std::string log_file_path = "";
+    std::string config_file_path = "";
+    while ((ch = getopt(argc,argv, "vlcd")) != EOF)
+    {
+        switch(ch)
+        {
+            case 'v':
+                usage();
+                ::exit(0);
+            case 'd':
+                run_in_foreground = true;
+                break;
+            case 'l':
+                assert(optarg != NULL);// this means we didn't declare getopt options correctly or there is a bug in getopt.
+                log_file_path = std::string(optarg);
+                break;
+            case 'c':
+                assert(optarg != NULL);// this means we didn't declare getopt options correctly or there is a bug in getopt.
+                config_file_path= std::string(optarg);
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (!run_in_foreground)
+    {
+        daemon(0, 0);
+    }
+
+    if(!Logger::instance()->init(log_file_path))
     {
         printf("init log module failed.\n");
         return -2;
     }
-    else
-    {
-        printf("init log module success!");
-    }
 
     Iniconfig config;
-
-    if (!config.loadfile(std::string(argv[2])))
+    if (!config.loadfile(config_file_path))
     {
         LOG_ERROR("load %s failed.", argv[2]);
         return -3;
@@ -78,14 +275,20 @@ int main(int argc, char** argv)
     {
         LOG_INFO("parent id is %d", getpid());
         LOG_INFO("brks start successful!");
+
+        int wstatus = 0;
+        //pid_t pid = wait(wstatus);
+
     }
     else if (processid == 0)
     {
         LOG_INFO("this is child process pid=%d", getpid());
         intf.start(server_socket);
+        LOG_ERROR("child process exit");
     }
 
     for(;;);
 
     return 0;
 }
+
